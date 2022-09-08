a787ff75fba6897e0b8ceb945200c7c87f1aa9bf
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-08 15:55:16 +0200

    5th challenge

---
+++ printcomb/printcomb.go
@@ -0,0 +1,22 @@
+package printcomb
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+
+	for a := 0; a != 10; a++ {
+		for b := 1; b != 10; b++ {
+			for c := 2; c != 10; c++ {
+				if a < b && b < c {
+					z01.PrintRune(rune(48 + a))
+					z01.PrintRune(rune(48 + b))
+					z01.PrintRune(rune(48 + c))
+					if a != 7 {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+}
---
+++ test/printcombtest.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"piscine/printcomb"
+)
+
+func main() {
+	printcomb.PrintComb()
+}
