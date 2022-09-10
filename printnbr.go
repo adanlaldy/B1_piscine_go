64b6433f4960d105fc73e20226ecf79d80f846a9
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-10 17:30:09 +0200

    7th challenge

---
+++ printnbr.go
@@ -0,0 +1,17 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+
+	var nb int = n
+
+	if nb > 10 {
+		PrintNbr(nb / 10)
+	} else if nb < 0 {
+		z01.PrintRune(rune(45))
+		nb = -nb
+		PrintNbr(nb / 10)
+	}
+	z01.PrintRune(rune((nb % 10) + 48))
+}
---
+++ printnbr/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"piscine"
+)
+
+func main() {
+	piscine.PrintNbr(-123)
+	piscine.PrintNbr(0)
+	piscine.PrintNbr(123)
+	z01.PrintRune('\n')
+}
