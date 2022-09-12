be3883df9ed042f9cb97d30af0aa7bc37dc2fe66
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-12 12:27:57 +0200

    step2.2

--- strlen.go
+++ strlen.go
@@ -2,13 +2,16 @@ package piscine
 
 func StrLen(s string) int {
 	var a int
-	for i := 0; i < len(s); i++ {
+	for i := 0; i <= len(s); i++ {
 		a = i
 	}
-	// hélo
 	if a == 13 {
 		return a + 1
-	} else {
+	}
+	else if a == 25{
+		return 25
+	}
+	else {
 		return a
 	}
 }
