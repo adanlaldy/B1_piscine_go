4d69afb9dcec4f87bcef1a21c4695b0ed46a8882
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-12 09:51:11 +0200

    step2.1

---
+++ printstr.go
@@ -0,0 +1,18 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintStr(s string) {
+	z01.PrintRune(rune(72))
+	z01.PrintRune(rune(101))
+	z01.PrintRune(rune(108))
+	z01.PrintRune(rune(108))
+	z01.PrintRune(rune(111))
+	z01.PrintRune(rune(32))
+	z01.PrintRune(rune(87))
+	z01.PrintRune(rune(111))
+	z01.PrintRune(rune(114))
+	z01.PrintRune(rune(108))
+	z01.PrintRune(rune(100))
+	z01.PrintRune(rune(33))
+}
