8130edd468ccf83d7953f51975ed23ffb5cb9b6d
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-13 16:44:48 +0200

    step3.1

---
+++ atoi.go
@@ -0,0 +1,20 @@
+package piscine
+
+func Atoi(s string) int {
+	a := 0
+	b := 1
+	if int(s[0]) == 45 {
+		b = -1
+	}
+	for i := 0; i < len(s); i++ {
+		if i == 0 && int(s[0]) == 45 || i == 0 && int(s[0]) == 43 {
+			continue
+		}
+		if int(s[i]) < 48 || int(s[i]) > 57 {
+			return 0
+		} else {
+			a = a*10 + (int(s[i]) - 48)
+		}
+	}
+	return a * b
+}
