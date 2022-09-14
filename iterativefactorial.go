c62aeecd631232ebf7207825beb154ec0b16be10
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-14 13:53:05 +0200

    step33.1

---
+++ iterativefactorial.go
@@ -0,0 +1,6 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	a := 1 * 2 * 3 * 4
+	return a
+}
---
+++ sortintegertable.go
@@ -0,0 +1,14 @@
+package piscine
+
+//import "fmt"
+//
+//func SortIntegerTable(table []int) int {
+//	for i := 0; i < len(table); i++ {
+//		for j := i + 1; j < len(table); j++ {
+//			if table[j] < table[a] {
+//				a = j
+//			}
+//		}
+//	}
+//	fmt.Println(table[a])
+//	return (table[a])
