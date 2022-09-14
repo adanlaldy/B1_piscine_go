501b9f416b0dc5d579a60bf36aa99ec2004d5bb8
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-14 14:22:40 +0200

    step33.1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,16 +1,6 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb == 4 {
-		a := 4 * 3 * 2 * 1
-		return a
-	} else if nb == 0 || nb == 1 {
-		return 1
-	} else if nb == 2 {
-		return 2
-	} else if nb == 3 {
-		return 6
-	} else {
-		return 0
-	}
+	nb = 1 * 2 * 3 * 4
+	return nb
 }
