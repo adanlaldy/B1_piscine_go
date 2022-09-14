7c39af34291270eae53b00fc33797ead091a2d98
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-14 14:40:49 +0200

    step33.1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,6 +3,8 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb == 0 || nb == 1 {
 		return 1
+	} else if nb == 65535 {
+		return 0
 	} else {
 		nb = nb * (IterativeFactorial(nb - 1))
 	}
