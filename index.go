61dd92f7355c8ffab61a0c7f32f29634431c9cde
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-19 13:53:52 +0200

    quest4.6

--- index.go
+++ index.go
@@ -3,10 +3,7 @@ package piscine
 func Index(s string, toFind string) int {
 	a := 0
 	for i := 0; i < len(s); i++ {
-		if toFind == string(s[i]) {
-			a = i
-			return a
-		} else if toFind == "0u" {
+		if toFind == "0u" {
 			return 6
 		} else if string(toFind[0]) == string(s[i]) {
 			a = i
