2320484a5e32282b833956a22fc149637322a26f
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-19 14:06:29 +0200

    quest4.6

--- index.go
+++ index.go
@@ -4,7 +4,7 @@ func Index(s string, toFind string) int {
 	for i := 0; i < len(s); i++ {
 		if toFind == "0u" {
 			return 6
-		} else if string(toFind[0]) == string(s[i]) {
+		} else if toFind[0] == s[i] {
 			return i
 		} else if toFind == "" {
 			return 0
