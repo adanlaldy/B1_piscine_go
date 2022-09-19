ca66f3d57c73828d5cac88ef2f14280e4ca8b16e
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-19 13:44:23 +0200

    quest4.6

--- index.go
+++ index.go
@@ -4,11 +4,9 @@ func Index(s string, toFind string) int {
 	a := 0
 	for i := 0; i < len(s); i++ {
 		if toFind == string(s[i]) {
-			return i
 			a = i
 			return a
 		} else if string(toFind[0]) == string(s[i]) {
-			return i
 			a = i
 			return a
 		} else if toFind == "" {
