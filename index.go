5b5d7c98a218d69d21ff851164d9feab2901736b
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-19 14:15:07 +0200

    quest4.6

--- index.go
+++ index.go
@@ -2,18 +2,8 @@ package piscine
 
 func Index(s string, toFind string) int {
 	for i := 0; i < len(s); i++ {
-		if toFind == "0u" {
-			return 6
-		} else if toFind[0] == s[i] {
+		if toFind[0] == s[i] {
 			return i
-		} else if toFind == "" {
-			return 0
-		} else if toFind == "lN" {
-			return 3
-		} else if toFind == ";9" {
-			return 2
-		} else if toFind == ",uI<`" {
-			return 4
 		}
 	}
 	return -1
