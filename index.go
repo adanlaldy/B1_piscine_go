a4dc20b3bc41e57a33777be1168c288695f42e87
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-19 12:03:04 +0200

    quest4.6

---
+++ index.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	a := 0
+	for i := 0; i < len(s); i++ {
+		if toFind == string(s[i]) {
+			return i
+			a = i
+		} else if string(toFind[0]) == string(s[i]) {
+			return i
+			a = i
+		} else if string(toFind[0]) <= string(s[i]) {
+			return -1
+		}
+	}
+	return a
+}
