af0088249ea887dccd7e067b18a1e1bfa2f5c4b8
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-19 15:31:49 +0200

    quest4.6

--- index.go
+++ index.go
@@ -1,18 +1,33 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	if toFind == "" {
-		return 0
-	} else if toFind == "LNa:O=9~" {
-		return 3
+	length := 0
+	sublength := 0
+
+	for i := range s {
+		length = i + 1
 	}
-	S := AlphaCount(s)
-	I := AlphaCount(toFind)
-	for i := 0; I <= S; i++ {
-		if Compare(s[i:I], toFind) == 0 {
+
+	for i := range toFind {
+		sublength = i + 1
+	}
+
+	t := 0
+
+	for i := 0; i < length; i++ {
+		j := 0
+		t = i
+		for j < sublength {
+			if t < length && s[t] == toFind[j] {
+				j++
+				t++
+			} else {
+				break
+			}
+		}
+		if j == sublength {
 			return i
 		}
-		I++
 	}
 	return -1
 }
