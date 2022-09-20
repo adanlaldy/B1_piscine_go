e4ec00b9ebb23315b48d958004e53e5ae4ca4d93
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-20 14:29:50 +0200

    quest5.5

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,14 +1,16 @@
 package piscine
 
-//import "fmt"
-//
-//func SortIntegerTable(table []int) int {
-//	for i := 0; i < len(table); i++ {
-//		for j := i + 1; j < len(table); j++ {
-//			if table[j] < table[a] {
-//				a = j
-//			}
-//		}
-//	}
-//	fmt.Println(table[a])
-//	return (table[a])
+import "fmt"
+
+func SortIntegerTable(table []int) int {
+	a := 0
+	for i := 0; i < len(table); i++ {
+		for j := i + 1; j < len(table); j++ {
+			if table[j] < table[a] {
+				a = j
+			}
+		}
+	}
+	fmt.Println(table[a])
+	return (table[a])
+}
