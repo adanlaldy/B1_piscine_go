3e7d70909c3e11c7926edebae2a11424e661e72b
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-20 09:27:14 +0200

    quest5.2

--- isprintable.go
+++ isprintable.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsPrintable(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if int(s[i]) >= 48 && int(s[i]) <= 57 || int(s[i]) >= 97 && int(s[i]) <= 122 || int(s[i]) >= 65 && int(s[i]) <= 90 {
+		if int(s[i]) >= 33 && int(s[i]) <= 126 || int(s[i]) >= 128 && int(s[i]) <= 254 {
 			continue
 		} else {
 			return false
