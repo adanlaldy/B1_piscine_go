f46452b399bfc45d93964c58f947a69e83daab95
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-21 15:24:33 +0200

    quest5.7

--- capitalize.go
+++ capitalize.go
@@ -3,9 +3,7 @@ package piscine
 func Capitalize(s string) string {
 	slice := []rune(s)
 	for i := 0; i < len(s); i++ {
-		if rune(s[i]) >= 97 && rune(s[i]) <= 122 {
-			continue
-		} else if rune(s[i]) >= 97 && rune(s[i]) <= 122 && (rune(s[i-1]) >= 65 && rune(s[i-1]) <= 90 || rune(s[i-1]) == 32 || rune(s[i-1]) == 43) {
+		if rune(s[i]) >= 97 && rune(s[i]) <= 122 && !(rune(s[i-1]) >= 97 && rune(s[i-1]) <= 122) && !(rune(s[i-1]) >= 48 && rune(s[i-1]) <= 57) && !(rune(s[i-1]) >= 65 && rune(s[i-1]) <= 90) {
 			slice[i] = rune(s[i]) - 32
 		}
 	}
