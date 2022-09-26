a0268608e05a2efe7e451123e12ce43a72ec616d
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-26 17:28:03 +0200

    6.4

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,10 +3,15 @@ package piscine
 func SplitWhiteSpaces(s string) []string {
 	slice := []string{}
 	char := ""
+	if len(s) == 0 {
+		return slice
+	}
 	for i := 0; i < len(s); i++ {
 		if s[i] == ' ' || s[i] == '\t' || s[i] == '\n' {
-			slice = append(slice, char)
-			char = ""
+			if len(char) != 0 {
+				slice = append(slice, char)
+				char = ""
+			}
 		} else {
 			char += string(s[i])
 		}
