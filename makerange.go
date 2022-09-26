928340590f987154947ac6fd9bfd6a0463e689a7
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-26 14:52:24 +0200

    6.2

--- makerange.go
+++ makerange.go
@@ -1,14 +1,13 @@
 package piscine
 
 func MakeRange(min, max int) []int {
+	sliceempty := make([]int, 0)
+	if min >= max {
+		return sliceempty
+	}
 	slice := make([]int, max-min)
 	for i := 0; i < max-min; i++ {
 		slice[i] = min + i
-	}
-	if min < max {
-		return slice
-	} else if min >= max {
-		return nil
 	}
 	return slice
 }
