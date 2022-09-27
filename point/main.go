12b2e80835d7073de337afd2b0230db9de46d742
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-27 11:24:05 +0200

    7.2

---
+++ point/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+type point struct {
+	x, y rune
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune(points.x/10 + 48)
+	z01.PrintRune(points.x%10 + 48)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune(points.y/10 + 48)
+	z01.PrintRune(points.y%10 + 48)
+	z01.PrintRune('\n')
+}
