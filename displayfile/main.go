253cdc069c47fcec09b5eb9580b5c36b8bcdd9df
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-28 10:00:15 +0200

    7..3

--- displayfile/main.go
+++ displayfile/main.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Println("File name missing")
+		fmt.Print("File name missing")
 		return
 	} else if len(os.Args) > 2 {
-		fmt.Println("Too many arguments")
+		fmt.Print("Too many arguments")
 	} else {
 		a, _ := os.ReadFile("quest8.txt")
-		fmt.Println(string(a))
+		fmt.Print(string(a))
 	}
 }
