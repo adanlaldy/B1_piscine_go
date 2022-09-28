83071167a75403d0ddbafb677adb65062d90a0ca
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-28 16:26:10 +0200

    8.3

--- any.go
+++ any.go
@@ -2,12 +2,10 @@ package piscine
 
 func Any(f func(string) bool, a []string) bool {
 	var b bool
-	var c bool
 	for i := 0; i < len(a); i++ {
 		b = IsNumeric(a[i])
 		if IsNumeric(a[i]) == true {
-			c = IsNumeric(a[i])
-			return c
+			return IsNumeric(a[i])
 		}
 	}
 	return b
