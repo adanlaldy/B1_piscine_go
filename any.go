15e38e421bbef2fd21677024973c019dac827950
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-28 16:44:42 +0200

    8.3

--- any.go
+++ any.go
@@ -3,9 +3,9 @@ package piscine
 func Any(f func(string) bool, a []string) bool {
 	var b bool
 	for i := 0; i < len(a); i++ {
-		b = IsNumeric(a[i])
-		if IsNumeric(a[i]) == true {
-			return IsNumeric(a[i])
+		b = f(a[i])
+		if f(a[i]) == true {
+			return f(a[i])
 		}
 	}
 	return b
--- isnumeric.go
+++ isnumeric.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsNumeric(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if int(s[i]) >= 48 && int(s[i]) <= 57 || int(s[i]) >= 97 && int(s[i]) <= 122 {
+		if int(s[i]) >= 48 && int(s[i]) <= 57 {
 			continue
 		} else {
 			return false
