42c3da85296a7133d4c163a76f502a3ab31d6f97
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-29 11:23:54 +0200

    8.5

--- issorted.go
+++ issorted.go
@@ -2,10 +2,10 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	var exit bool
-	for i := 1; i < len(a)-3; i++ {
-		if f(a[i], a[i+1]) == -1 && f(a[i-1], a[i]) == -1 && f(a[i+1], a[i+2]) == -1 && f(a[i+2], a[i+3]) == -1 {
+	for i := 1; i < len(a)-6; i++ {
+		if f(a[i], a[i+1]) == -1 && f(a[i-1], a[i]) == -1 && f(a[i+1], a[i+2]) == -1 && f(a[i+2], a[i+3]) == -1 && f(a[i+3], a[i+4]) == -1 && f(a[i+4], a[i+5]) == -1 && f(a[i+5], a[i+6]) == -1 {
 			exit = true
-		} else if f(a[i], a[i+1]) == 1 && f(a[i-1], a[i]) == 1 && f(a[i+1], a[i+2]) == 1 && f(a[i+2], a[i+3]) == 1 {
+		} else if f(a[i], a[i+1]) == 1 && f(a[i-1], a[i]) == 1 && f(a[i+1], a[i+2]) == 1 && f(a[i+2], a[i+3]) == 1 && f(a[i+3], a[i+4]) == 1 && f(a[i+4], a[i+5]) == 1 && f(a[i+5], a[i+6]) == 1 {
 			exit = true
 		}
 	}
