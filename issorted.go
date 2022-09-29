5cb188fdf0abbfb65738f9521339e849f06744ae
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-29 11:12:27 +0200

    8.5

--- issorted.go
+++ issorted.go
@@ -2,18 +2,14 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	var exit bool
-	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) == -1 {
+	for i := 1; i < len(a); i++ {
+		if f(a[i], a[i+1]) == -1 && f(a[i-1], a[i]) == -1 {
+			exit = true
+		}
+		if f(a[i], a[i+1]) == 1 && f(a[i-1], a[i]) == 1 {
 			exit = true
 		} else {
 			break
-		}
-		for j := 0; j < len(a)-1; j++ {
-			if f(a[j], a[j+1]) == 1 {
-				exit = true
-			} else {
-				break
-			}
 		}
 	}
 	return exit
