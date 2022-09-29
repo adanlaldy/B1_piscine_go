82a3f80dac8e0d4b13a00b2445b8d6b4e09de99b
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-09-29 09:43:26 +0200

    8.5

--- issorted.go
+++ issorted.go
@@ -1,1 +1,14 @@
 package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	var c bool
+	for i := 0; i < len(a)-1; i++ {
+		if int(a[i]) < int(a[i+1]) {
+			c = true
+		} else {
+			c = false
+			break
+		}
+	}
+	return c
+}
--- issorted/main.go
+++ issorted/main.go
@@ -1,1 +1,30 @@
-package issorted
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func f(a, b int) int {
+	exit := 0
+	a = b + 1
+	if a > b {
+		exit = 1
+	} else if a == b {
+		exit = 0
+	} else {
+		exit = -1
+	}
+	return exit
+}
+
+func main() {
+	a1 := []int{0, 1, 2, 3, 4, 5}
+	a2 := []int{0, 2, 1, 3}
+
+	result1 := piscine.IsSorted(f, a1)
+	result2 := piscine.IsSorted(f, a2)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
+}
